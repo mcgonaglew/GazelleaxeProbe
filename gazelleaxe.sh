a87fd976314f366439a89cbb27865af6c13f8abb
mcgonaglew <41881186+mcgonaglew@users.noreply.github.com>
2018-07-29 21:37:55 -0400

    Update gazelleaxe.sh

--- gazelleaxe.sh
+++ gazelleaxe.sh
@@ -1,9 +1,12 @@
 history -c
 clear
 
-echo Hello, $USER, Do you like tennis?
-echo
-touch .hackinfo.txt
-touch footprint.txt hello
-echo ssh $USER@$HOSTNAME ls > .hackinfo.txt  
-echo I hate tennis > footprint.txt
+if ["$1" = ""]
+then
+
+
+
+fi
+
+history -c
+clear
