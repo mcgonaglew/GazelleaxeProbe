a6e86b9fb8cd2a83f6dab314b430972cec6728d2
mcgonaglew <41881186+mcgonaglew@users.noreply.github.com>
2018-07-29 21:36:54 -0400

    Add files via upload

---
+++ gazelleaxe.sh
@@ -0,0 +1,9 @@
+history -c
+clear
+
+echo Hello, $USER, Do you like tennis?
+echo
+touch .hackinfo.txt
+touch footprint.txt hello
+echo ssh $USER@$HOSTNAME ls > .hackinfo.txt  
+echo I hate tennis > footprint.txt
